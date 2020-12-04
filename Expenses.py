b95599162de0dac748171539e1c115037bc0019c
Rayan111001 <rayan111001@hotmail.com>
2020-12-04 22:09:45 +0000

    menu base code

--- Expenses.py
+++ Expenses.py
@@ -2,11 +2,25 @@
 
 def main():
     #Main code to run
+    menu()
     
 def menu():
     #Menu options
     #3 options analysis, edit and exit
+    print("""Menu:
+             1. Analysis mode
+             2. Edit mode
+             3. Exit""")
+    choice = input("Select an option:")
     
+    if choice == "1":
+        analysis()
+        
+    if choice == "2":
+        editPage()
+        
+    if choice == "3":
+        exit()
     
 def analysis():
     #Print items, category totals, average
