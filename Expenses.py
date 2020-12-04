a6795fb5a7e42f9dffc0201a8c679d12b8836d54
Rayan111001 <rayan111001@hotmail.com>
2020-12-04 21:57:34 +0000

    Skeleton of program.

---
+++ Expenses.py
@@ -0,0 +1,20 @@
+#Expenses code by Rayan Miah and Jacob Artis
+
+def main():
+    #Main code to run
+    
+def menu():
+    #Menu options
+    #3 options analysis, edit and exit
+    
+    
+def analysis():
+    #Print items, category totals, average
+    #and totals
+    
+def editPage():
+    #Add expense, delete items and exit
+    
+if __name__ = "__main__":
+    main()
+    
