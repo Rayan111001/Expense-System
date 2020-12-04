9811a619d454a6a4fc93fd365ef2826a4f1a1c7e
jacobartis <zjac219@cim-ts-node-01.cc.rhul.local>
2020-12-04 22:40:52 +0000

    added menu functionality to the edit page.

--- Expenses.py
+++ Expenses.py
@@ -23,13 +23,38 @@ def menu():
         exit()
     
 def analysis():
+    print("no")
     #Print items, category totals, average
     #and totals
     
+def addExpense():
+    print ("To add an expense please give the folowing information")
+    newDesc = input("Add a small description of the new expense: ")
+    newCategory = input("Add a category for the new expense: ")
+    newAmount = input("Give the amount of the new expense: ")
+
+def deleteExpense():
+    print("no")
+    
 def editPage():
     #Add expense, delete items and exit
+    while True:
+        print("""Menu:
+            1. Add expence
+            2. Delete expence
+            3. Exit""")
+        editChoice = input("Select an option:")
+        
+        if editChoice == "1":
+            addExpense()
+        
+        if editChoice == "2":
+            deleteExpense()
+        if editChoice == "3":
+            break
     
-if __name__ = "__main__":
+    
+if __name__ == "__main__":
     #Runs the program
     main()
     
